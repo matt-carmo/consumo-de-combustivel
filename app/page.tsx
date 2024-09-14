ea545a47eb19cad502b7f8275375ae4931913397
matt-carmo <matheus2018i@gmail.com>
2024-09-14 18:32:50 -0300

    Update visual

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-import Image from "next/image";
 import { useState } from "react";
 
 
